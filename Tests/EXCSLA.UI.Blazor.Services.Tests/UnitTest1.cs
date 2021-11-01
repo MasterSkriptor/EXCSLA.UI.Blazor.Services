834f23a4cff4138c8052b271777e2ecdd6a111d1
Harold E Collins <masterskriptor@hotmail.com>
2021-11-01 07:17:02 -0500

    set up initial project, added prewritten code, set tests and test viewing project.

---
+++ Tests/EXCSLA.UI.Blazor.Services.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EXCSLA.UI.Blazor.Services.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/EXCSLA.UI.Blazor.Services/ReportBarService/IReportBarFilterCreator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EXCSLA.UI.Blazor.Services
+{
+    public interface IReportBarFilterCreator
+    {
+        Dictionary<string, int> CreateFilters<TItem>(List<TItem> itemsToFilter, List<string> filters, string field);
+    }
+}
