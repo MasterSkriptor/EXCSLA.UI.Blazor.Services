d32a2f53f31996320dbb505e48f0ce71a2ae541a
Harold E Collins <masterskriptor@hotmail.com>
2021-11-04 09:39:59 -0500

    Added extension to register alert service for dependency injection.

---
+++ src/EXCSLA.UI.Blazor.Services/AlertService/AlertServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCSLA.UI.Blazor.Services.AlertService
+{
+    public static class AlertServiceExtensions
+    {
+        public static void AddAlertService(this IServiceCollection services)
+        {
+            services.AddSingleton<IAlertService, AlertService>();
+        }
+    }
+}
